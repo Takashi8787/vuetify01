cd1ed143b525fe79b06166769872d38f00f1edf4
Takashi8787 <kodawari810@gmail.com>
2019-05-30 18:52:54 +0900

    20190530_17:00

--- vuetify01/src/App.vue
+++ vuetify01/src/App.vue
@@ -1,27 +1,35 @@
 <template>
   <v-app>
-    <v-toolbar app>
+    <!-- <v-toolbar app>
       <v-toolbar-side-icon @click.stop="toggleSideMenu"></v-toolbar-side-icon>
       <v-toolbar-title class="headline text-uppercase">
         <span>マイアドレス帳</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-    </v-toolbar>
-    <SideNav/>
+    </v-toolbar> -->
 
+    <!-- <br>
+    <br> -->
+
+    <v-btn color="success">aaa</v-btn>
+    <!-- <SideNav/> -->
+
+    <!-- Practiceコンポーネントの表示 -->
     <v-content>
-      <router-view/>
+      <Practice/>
     </v-content>
   </v-app>
 </template>
 
 <script>
 import SideNav from './components/SideNav'
+import Practice from './components/Practice'
 import { mapActions } from 'vuex'
 export default {
   name: 'App',
   components: {
-    SideNav
+    SideNav,
+    Practice
   },
   data () {
     return {
--- vuetify01/src/components/Practice.vue
+++ vuetify01/src/components/Practice.vue
@@ -1,10 +1,17 @@
 <template>
-  <v-container>
-    <v-layout text-xs-center wrap>
-        <p>コンポーネントSUBです</p>
-        <p>{{ message }}</p>
-    </v-layout>
-  </v-container>
+  <v-app>
+    <v-content wrap style="width: 500px;">
+
+        <h1>ToDoList</h1>
+        <input type="radio">全て
+        <input type="radio">作業中
+        <input type="radio">完了
+        <br>
+        <v-btn color="success">BBB!</v-btn>
+        <p>p要素</p>
+        <div>div要素</div>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
