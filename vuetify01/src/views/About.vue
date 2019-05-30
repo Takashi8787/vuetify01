186b09381719b89d90c1772a6abbd069ebe9b9dc
Takashi8787 <kodawari810@gmail.com>
2019-05-30 21:38:46 +0900

    2019

--- vuetify01/src/views/About.vue
+++ vuetify01/src/views/About.vue
@@ -1,13 +1,16 @@
 <template>
-  <NengaTop />
+  <div class="about">
+    <h1>This is an about pageですよ！</h1>
+  </div>
 </template>
 
-<script>
-  console.log("tootta");
-  import NengaTop from '../components/NengaTop'
-  export default {
-    components: {
-      NengaTop
-    }
-  }
-</script>
+
+// <script>
+//   console.log("tootta");
+//   import NengaTop from '../components/NengaTop'
+//   export default {
+//     components: {
+//       NengaTop
+//     }
+//   }
+// </script>
