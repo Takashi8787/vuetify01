29d52c02a305151e8f1ded68ffbcdaaa148ccf7b
Takashi8787 <kodawari810@gmail.com>
2019-05-30 14:58:43 +0900

    20190530_base

---
+++ vuetify01/src/components/Practice.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <v-layout text-xs-center wrap>
+        <p>コンポーネントSUBです</p>
+        <p>{{ message }}</p>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+        message: "NANA",  
+    }),
+    mounted: function() {
+        console.log("mountedのコンソール");
+    },  
+  }
+</script>
+
+<style>
+
+</style>
---
+++ vuetify01/src/views/Sub.vue
@@ -0,0 +1,12 @@
+<template>
+  <Practice />
+</template>
+
+<script>
+  import Practice from '../components/Practice'
+  export default {
+    components: {
+      Practice
+    }
+  }
+</script>
